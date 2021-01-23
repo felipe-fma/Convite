819453266151a6d305098d8eabc7a43414b362da
Felipe de moura arantes <felipe.moura@dmcard.com.br>
2021-01-23 14:08:53 -0300

    Alterando cor do cartao e o evendo de abertura

--- envelope.js
+++ envelope.js
@@ -9,7 +9,9 @@
         top: document.getElementById('envelopeTop'),
         left: document.getElementById('envelopeBottomRight'),
         bottomRight: document.getElementById('envelopeTop'),
+        right: document.getElementById('envelopeLeft'),
         paper: document.getElementById('paper'),
+        letter: document.getElementById('contact'),
 
         //Object to record state
         state: {
@@ -19,6 +21,9 @@
             topRotation: 0,
             size: 1,
             isFullOpen: false,
+            RotationLetter: 0,
+            transY: 0,
+
         },
     };
 
@@ -51,8 +56,6 @@
     closeEnvelope.clear = undefined;
 
     function openEnvelope() {
-        console.log("Open");
-
         //Remove invalid Event Listeners and add proper ones
         rockit.cancel();
         envelope.body.removeEventListener('mouseover', openEnvelope);
@@ -64,34 +67,79 @@
         }
 
         if (envelope.state.topRotation > 180) {
-            envelope.paper.style.zIndex = 6;
-            envelope.top.style.zIndex = 0;
-            envelope.left.style.zIndex = 0;
-            envelope.bottomRight.style.zIndex = 0;
-
             clearTimeout(openEnvelope.clear);
-            sizeUp()
+            translateYNegativo()
 
         } else {
             //Change the step increment of rotate to adjust smoothness 
             envelope.state.topRotation += 2;
             //Apply the changes
-            envelope.top.style.transform = "rotateX" + "(" + envelope.state.topRotation + "deg" + ")";
+            envelope.top.style.transform = "rotateX" + "(" + envelope.state.topRotation + "deg)";
             openEnvelope.clear = setTimeout(openEnvelope, 10);
         }
 
     };
 
+    function translateYNegativo() {
+        if (envelope.state.transY <= -200) {
+
+            envelope.top.style.zIndex = 0;
+            envelope.left.style.zIndex = 0;
+            envelope.right.style.zIndex = 0;
+            envelope.bottomRight.style.zIndex = 0;
+
+            envelope.letter.style.zIndex = 4;
+            envelope.paper.style.zIndex = 5;
+
+            clearTimeout(openEnvelope.clear);
+            translateYPositivo()
+        } else {
+            envelope.state.transY -= 5
+
+            envelope.paper.style.transform = "translateY(" + envelope.state.transY + "px)";
+            openEnvelope.clear = setTimeout(translateYNegativo, 10);
+        }
+    }
+
+
+    function translateYPositivo() {
+
+        if (envelope.state.transY >= 0) {
+            clearTimeout(openEnvelope.clear);
+            envelope.state.transY = 0;
+            console.log("")
+            sizeUp()
+        } else {
+            envelope.state.transY += 5
+
+            envelope.paper.style.transform = "translateY(" + envelope.state.transY + "px)";
+            openEnvelope.clear = setTimeout(translateYPositivo, 10);
+        }
+    }
+
     function sizeUp() {
         if (envelope.state.size > 2.5) {
-            envelope.state.isEnvelopeOpen = true;
+
             clearTimeout(openEnvelope.clear);
+            envelope.state.isEnvelopeOpen = true;
+            //RotateLetter()
         } else {
             envelope.state.size += 0.05
 
             envelope.paper.style.transform = "scale(" + envelope.state.size + ")";
-            console.log("paper trans " + envelope.state.size);
             openEnvelope.clear = setTimeout(sizeUp, 10);
+        }
+    }
+
+    function RotateLetter() {
+        if (envelope.state.RotationLetter == 0) {
+            envelope.state.isEnvelopeOpen = true;
+            clearTimeout(closeEnvelope.clear);
+
+        } else {
+            envelope.state.RotationLetter += 1
+            envelope.letter.style.transform = "rotate(" + envelope.state.RotationLetter + "deg)";
+            closeEnvelope.clear = setTimeout(RotateLetterInverse, 10);
         }
     }
 
@@ -109,13 +157,9 @@
 
         if (envelope.state.isEnvelopeOpen) {
             if (envelope.state.size <= 1) {
-                envelope.paper.style.zIndex = 0;
-                envelope.top.style.zIndex = 1;
-                envelope.left.style.zIndex = 1;
-                envelope.bottomRight.style.zIndex = 1;
-
                 clearTimeout(closeEnvelope.clear);
-                InvertRotateX()
+
+                translateYNegativoInverse()
 
             } else {
                 //Change the step increment of rotate to adjust smoothness 
@@ -127,6 +171,41 @@
             }
         }
     };
+
+    function translateYNegativoInverse() {
+        if (envelope.state.transY <= -200) {
+
+            envelope.top.style.zIndex = 1;
+            envelope.left.style.zIndex = 2;
+            envelope.right.style.zIndex = 3;
+            envelope.bottomRight.style.zIndex = 4;
+
+            envelope.letter.style.zIndex = 0;
+            envelope.paper.style.zIndex = 0;
+
+            clearTimeout(closeEnvelope.clear);
+            translateYPositivoInverse()
+        } else {
+            envelope.state.transY -= 5
+
+            envelope.paper.style.transform = "translateY(" + envelope.state.transY + "px)";
+            closeEnvelope.clear = setTimeout(translateYNegativoInverse, 10);
+        }
+    }
+
+    function translateYPositivoInverse() {
+
+        if (envelope.state.transY >= 0) {
+            clearTimeout(closeEnvelope.clear);
+            envelope.state.transY = 0;
+            InvertRotateX();
+        } else {
+            envelope.state.transY += 5
+
+            envelope.paper.style.transform = "translateY(" + envelope.state.transY + "px)";
+            closeEnvelope.clear = setTimeout(translateYPositivoInverse, 10);
+        }
+    }
 
     function InvertRotateX() {
         if (envelope.state.topRotation <= 0) {
